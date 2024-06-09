cf09efd8ef03a9f1e8f31c84233326e3409bd227
Narasimharao <narasimhapasumarthi5@gmail.com>
2024-06-09 23:04:44 +0530

    distadded

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-var _reactDom = _interopRequireDefault(require("react-dom"));
-var _app = _interopRequireDefault(require("./components/app"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-_reactDom["default"].render( /*#__PURE__*/_react["default"].createElement(_app["default"], null), document.getElementById("root"));
